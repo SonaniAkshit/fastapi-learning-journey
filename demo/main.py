ccb7e2cedb54c285777dc532e08ee950344df1cb
SonaniAkshit <sonaniakshit684@gmail.com>
2026-03-31 21:43:30 +0530

    Add basic FastAPI routes with path and query parameters
    Implemented initial FastAPI app with multiple GET endpoints including root, about, and reload routes.
    Added dynamic route with path and optional query parameters for item retrieval.

---
+++ demo/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {'message':'Hello World'}
+
+@app.get("/about")
+def about():
+    return{'message':'Hello i m akshit i curruntly Learning AI'}
+
+@app.get("/reload")
+def reload():
+    return{'message':'hello'}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
