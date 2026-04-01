99a11efc6b5ec16caed595b584b4c84287c18621
SonaniAkshit <sonaniakshit684@gmail.com>
2026-04-01 14:06:38 +0530

    Update items endpoint to use item_name query parameter
    Modified /items/{item_id} endpoint to replace q query parameter with item_name.
    Updated response structure to reflect new parameter usage.

--- demo/main.py
+++ demo/main.py
@@ -15,5 +15,5 @@ def reload():
     return{'message':'hello'}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int, item_name: str | None = None):
+    return {"item_id": item_id, "item_name": item_name}
